161709ef2b283b8dd9db7e238ec1e4a41e718051
louiseprj <poulalouiserj@gmail.com>
2019-08-15 23:12:13 +0800

    Second Commit

---
+++ src/Activity2.java
@@ -0,0 +1,45 @@
+//Poula Jose//
+
+import java.util.Scanner;
+ 
+public class Activity2 {
+    
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    	
+    	String Name;
+    	String Course;
+    	String Section;
+    	String Address;
+    	String ContactNumber;
+    	
+    	
+    	System.out.println("What is your Name?");
+    	Name = scan.nextLine ();
+    	
+    	System.out.println("\nYour Name is " + Name);
+    	
+    	System.out.println("\nWhat is your Course?");
+    	Course = scan.nextLine ();
+    	
+    	System.out.println("\nYour Course is " + Course);
+    	
+    	System.out.println("\nWhat is your Ssection?");
+    	Section = scan.nextLine ();
+    	
+    	System.out.println("\nYour Section is " + Section);
+    	
+    	System.out.println("\nWhat is your Address?");
+    	Address = scan.nextLine ();
+    	
+    	System.out.println("\nYour Address is " + Address);
+    	
+    	System.out.println("\nWhat is your Contact Number?");
+    	ContactNumber = scan.nextLine ();
+    	
+    	System.out.println("Your Contact Number is " + ContactNumber);
+ 
+    }
+}
+
+
